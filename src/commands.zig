7c2ed55fce8f757c1aab10fd16ddf0024a2afc35
lyraplasma <274751584+lyraplasma@users.noreply.github.com>
2026-06-25 10:52:10 +0000

    feat:3 command handlers

---
+++ src/commands.zig
@@ -0,0 +1,34 @@
+const std = @import("std");
+const cli = @import("cli.zig");
+
+pub const methods = struct {
+    pub const commands = struct {
+        // Handler for the "hello" command
+        pub fn helloFn(_options: []const cli.option) bool {
+            std.debug.print("Hello, ", .{});
+
+            // Look for a "name" option
+            for (_options) |opt| {
+                // TODO: Investigate later where that opt.name lies
+                if (std.mem.eql(u8, opt.name, "name")) {
+                   if (opt.value.len > 0) {
+                      std.debug.print("{s}", .{opt.value});
+                   } else {
+                      std.debug.print("World", .{});
+                   }
+               break;
+            }
+        }
+       std.debug.print("!\n", .{});
+       return true;
+       }
+    };
+
+    pub const options = struct {
+        // Handler for the "name" option
+        pub fn nameFn(_: []const u8) bool {
+            // Option-specific logic should go here
+            return true;
+        }
+    };
+};
